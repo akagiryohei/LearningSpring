66ece3304190b8e447bc1ad99a55fc67ca2e7cba
ryohei_akagi <abbbbbbbb35@gmail.com>
2026-05-03 20:48:04 +0900

    No.39の内容を反映：Lombok

--- spring-boot-introduction-start-here/spring-boot-introduction-start-here/src/main/java/com/example/todo/service/task/TaskService.java
+++ spring-boot-introduction-start-here/spring-boot-introduction-start-here/src/main/java/com/example/todo/service/task/TaskService.java
@@ -1,7 +1,9 @@
 package com.example.todo.service.task;
 
-import java.util.List;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+@Service
 public class TaskService {
 
     public List<TaskEntity> find() {
